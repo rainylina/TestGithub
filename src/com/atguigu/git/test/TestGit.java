8b6f1404caca135301cbb16a99245ca436b11c16
rainylina <rainylina@163.com>
2016-03-18 19:43:56 +0800

    update

--- src/com/atguigu/git/test/TestGit.java
+++ src/com/atguigu/git/test/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello Github");
 		System.out.println("hello Github2");
-		
+		System.out.println("hello Github3");
 	}
 }
